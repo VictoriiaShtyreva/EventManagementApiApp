a89633ee9874b5f6d9cc3a40b8435f70f62b2500
Viktoriia <vikatori1409801@gmail.com>
2024-06-20 16:16:48 +0300

    Start project

--- Program.cs
+++ Program.cs
@@ -3,11 +3,11 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using EventManagementApi.Data;
 using Azure.Storage.Blobs;
 using Azure.Messaging.ServiceBus;
 using Microsoft.OpenApi.Models;
 using EventManagementApi.Entity;
+using EventManagementApi.Entities;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,25 +20,6 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
-    };
-});
 
 builder.Services.AddAuthorization();
 
